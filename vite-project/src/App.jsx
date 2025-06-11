0ee2fe9d82805597668c5c90d6c3300109281c07
Abbas Rabbani <mail.abbas.rabbani@gmail.com>
2025-06-11 18:56:41 +0200

    Using hooks useEffects

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Components
 
 const Card = ({ title }) => {
-
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
   return (
-    <div className='card'>
-      <h2>{title}</h2>
+    <div className='card' onClick={() => setCount(count + 1)}>
+      <h2>{title} <br /> {count}</h2>
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? '❤️' : '🖤'}
       </button>
