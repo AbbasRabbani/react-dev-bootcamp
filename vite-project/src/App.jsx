879c98b1816fac574a08ccf55886741ba438e41e
Abbas Rabbani <mail.abbas.rabbani@gmail.com>
2025-06-11 18:08:41 +0200

    working with some inline styles

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,7 +4,14 @@ import './App.css'
 
 const Card = ({ title }) => {
   return (
-    <div>
+    <div style={{
+      border: '1px solid #4b5362',
+      padding: '20px', margin: '10px',
+      borderRadius: '8px',
+      backgroundColor: '#31363f',
+      borderRadius: '10px',
+      minHeight: '200px'
+    }}>
       <h2>{title}</h2>
     </div>
   )
